f41ca198f2f3c9f43b4fd47ab5f0c6f61c7c7a4e
G Wei Lim <limgwei24@gmail.com>
2026-03-12 16:51:28 +0800

    adding testing search by id

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -11,3 +11,18 @@ def test_me_success(client, access_token):
     assert response.status_code == 200
     data = response.json()
     assert "email" in data
+
+def test_search_by_id(client, access_token,test_user):
+    me_response = client.get("profile/me", headers={
+            "Authorization": f"Bearer {access_token}"
+        })
+
+    assert me_response.status_code == 200
+    me_data = me_response.json()
+    user_id = me_data['id']
+
+    response = client.get(f"profile/{user_id}", headers={
+            "Authorization": f"Bearer {access_token}"
+        })
+    assert response.status_code == 200
+    assert response.json()["id"] == user_id
